97422cffd2121d39facde1f8264f838df375e65a
Narendhar <narendhar.mrit467@gmail.com>
2023-03-16 06:54:22 +0000

    first commit

---
+++ src/Components/HistoryView/index.js
@@ -0,0 +1,30 @@
+import './index.css'
+
+const HistoryView = props => {
+  const {history, deleteFun} = props
+  const {id, timeAccessed, logoUrl, title, domainUrl} = history
+
+  const onDeleteClick = () => {
+    deleteFun(id)
+  }
+
+  return (
+    <li className="historyBar">
+      <div className="part1">
+        <p className="time">{timeAccessed}</p>
+        <img className="Logo" src={logoUrl} alt="domain logo" />
+        <p className="title">{title}</p>
+        <p className="domainUrl">{domainUrl}</p>
+      </div>
+
+      <button type="button" data-testid="delete" className="deleteButton">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/delete-img.png"
+          alt="delete"
+          onClick={onDeleteClick}
+        />
+      </button>
+    </li>
+  )
+}
+export default HistoryView
